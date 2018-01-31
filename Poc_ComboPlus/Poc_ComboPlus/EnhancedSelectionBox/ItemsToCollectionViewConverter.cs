e141988095a42f8aa27ef2429c190dbcabe199e0
cconnes <cconnes@smag-group.com>
2018-01-31 12:41:53 +0100

    Ajout RadListBox virtualizée dans la popup de l'autocompletebox

---
+++ Poc_ComboPlus/Poc_ComboPlus/EnhancedSelectionBox/ItemsToCollectionViewConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Poc_ComboPlus
+{
+    [ValueConversion(typeof(IEnumerable), typeof(ICollectionView))]
+    public class ItemsToCollectionViewConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values != null && values.Length == 2)
+            {
+                var propertyName = values[1] as string;
+
+                var groupedCountries = new CollectionViewSource();
+                groupedCountries.GroupDescriptions.Add(new PropertyGroupDescription(propertyName));
+                groupedCountries.Source = values[0] as IEnumerable;
+
+                return groupedCountries.View;
+            }
+
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
