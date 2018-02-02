3afce2d2228705c40e039ef79752788cbafde44c
cconnes <cconnes@smag-group.com>
2018-02-02 10:24:04 +0100

    1ere version

---
+++ ObjectInCell/ObjectInCell/ObjectInCell/ViewModels.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace ObjectInCell
+{
+    public class MyViewModel : Telerik.Windows.Controls.ViewModelBase
+    {
+        public MyViewModel()
+        {
+            this.ItemsSource = new ObservableCollection<Row>()
+            {
+                new Row() { RowName = "Row 1" },
+                new Row() { RowName = "Row 2" },
+                new Row() { RowName = "Row 3" },
+            };
+        }
+
+        public ObservableCollection<Row> ItemsSource { get; set; }
+    }
+
+    public class Row
+    {
+        public Row()
+        {
+            this.Item = new Item();
+        }
+
+        public Item Item { get; private set; }
+        public string RowName { get; set; }
+    }
+
+    public class Item
+    {
+        private static Random rnd = new Random();
+
+        public Item()
+        {
+            this.Display = string.Format("Item {0}", rnd.Next(1, 999));
+            this.FireItem = new Telerik.Windows.Controls.DelegateCommand((object o) =>
+            {
+                System.Diagnostics.Debug.WriteLine("Double click on " + this.Display);
+            });
+        }
+
+        public string Display { get; set; }
+
+        public ICommand FireItem { get; set; }
+    }
+}
