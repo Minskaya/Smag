75e0e4b3691ea35347d15eb012e8c8f5ac06316d
cconnes <cconnes@smag-group.com>
2018-03-22 17:52:01 +0100

    Avec un converter qui marche un peu

--- DecimalMarkupExtension/DecimalMarkupExtension/DecimalPrecisionConverter.cs
+++ DecimalMarkupExtension/DecimalMarkupExtension/DecimalPrecisionConverter.cs
@@ -8,12 +8,25 @@ namespace DecimalMarkupExtension
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            double number = System.Convert.ToDouble(values[0]);
+            int precision = System.Convert.ToInt32(values[1]);
+
+            ScalingFactor scale;
+            Enum.TryParse<ScalingFactor>(parameter as string, out scale);
+
+            NumberScalingFormatter formatter = new NumberScalingFormatter(scale, CultureInfo.CurrentCulture);
+            return string.Format(
+                formatter,
+                "{0:N" + precision + "}",
+                number);
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
-            throw new NotImplementedException();
+            return new object[]
+            {
+                Binding.DoNothing
+            };
         }
     }
 }
