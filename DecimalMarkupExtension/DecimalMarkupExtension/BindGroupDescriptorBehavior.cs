cb31978a34b131e641a41b62a8cbce05567b101f
cconnes <cconnes@smag-group.com>
2018-03-23 16:30:00 +0100

    Combo pour regroupement ok

--- DecimalMarkupExtension/DecimalMarkupExtension/BindGroupDescriptorBehavior.cs
+++ DecimalMarkupExtension/DecimalMarkupExtension/BindGroupDescriptorBehavior.cs
@@ -1,7 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Interactivity;
 using Telerik.Windows.Controls;
-using Telerik.Windows.Data;
+using Telerik.Windows.Controls.GridView;
 
 namespace DecimalMarkupExtension
 {
@@ -9,17 +9,38 @@ namespace DecimalMarkupExtension
     {
         // Using a DependencyProperty as the backing store for GroupDescriptor.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty GroupDescriptorProperty =
-            DependencyProperty.Register("GroupDescriptor", typeof(GroupDescriptor), typeof(BindGroupDescriptorBehavior), new PropertyMetadata(null));
+            DependencyProperty.Register(
+                "GroupDescriptor",
+                typeof(ColumnGroupDescriptor),
+                typeof(BindGroupDescriptorBehavior),
+                new PropertyMetadata(
+                    null,
+                    new PropertyChangedCallback(OnGroupDescriptorsPropertyChanged)));
 
-        public GroupDescriptor GroupDescriptor
+        public ColumnGroupDescriptor GroupDescriptor
         {
-            get { return (GroupDescriptor)GetValue(GroupDescriptorProperty); }
+            get { return (ColumnGroupDescriptor)GetValue(GroupDescriptorProperty); }
             set { SetValue(GroupDescriptorProperty, value); }
         }
 
         protected override void OnAttached()
         {
             base.OnAttached();
+        }
+
+        private static void OnGroupDescriptorsPropertyChanged(DependencyObject dependencyObject, DependencyPropertyChangedEventArgs e)
+        {
+            var instance = dependencyObject as BindGroupDescriptorBehavior;
+            if (instance != null)
+            {
+                instance.UpdateGroupDescriptor();
+            }
+        }
+
+        private void UpdateGroupDescriptor()
+        {
+            this.AssociatedObject.GroupDescriptors.Clear();
+            this.AssociatedObject.GroupDescriptors.Add(this.GroupDescriptor);
         }
     }
 }
